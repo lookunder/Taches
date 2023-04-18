ae152a7df9585b1df6dfaf24dd8496cb9024e62f
Charles <charles.stpierre@gmail.com>
2023-04-18 14:32:21 -0700

    Added some tests.

--- test/TestTable.hs
+++ test/TestTable.hs
@@ -2,12 +2,12 @@
 
 module Main where
 
+import qualified Data.Text as T
 import Test.HUnit
 import Test.Framework
 import Test.Framework.Providers.HUnit
 import Data.Monoid
 import Control.Monad
---import Utils
 
 import Table
 
@@ -18,13 +18,30 @@ testMAppend = TestCase (do let t1 = TableAvecEntete [["d11","d12"],["d21", "d22"
                            assertEqual "Les deux tables sont différentes." t3 ref
                      )
 
+data Test0 = Test0 { int0 :: Integer, int1 :: Integer } deriving (Show)
+
+instance Aligner Test0 where
+  aligner (Test0 a b) = [(T.pack . show) a, (T.pack . show) b]
+
+instance Tabuler Test0 where
+    convertirAvecEntête ts = let table = map aligner ts
+                             in TableAvecEntete table ["Int 0", "Int 1"]
+
+testAlignement = TestCase (  do let t = Test0 1 2
+                                assertEqual "La fonction aligner ne fonctionne pas." ["1","2"] (aligner t)               
+                          )
+
+testTable = TestCase ( let t = Test0 1 2
+                           table = convertirAvecEntête [t]
+                       in assertEqual "La fonction convertirAvecEntête ne fonctionne pas." (TableAvecEntete [["1","2"]] ["Int 0", "Int 1"]) table              
+                    )
 --pushTest :: Assertion
 --pushTest = [NumLit 1] ^? push (NumLit 1)
 
 --pushPopTest :: Assertion
 --pushPopTest = [] ^? (push (NumLit 0) >> void pop)
-
-tests = TestList [TestLabel "<>" testMAppend]
+  
+tests = TestList [TestLabel "<>" testMAppend, testAlignement, testTable]
 
 main :: IO ()
 main = runTestTTAndExit tests
